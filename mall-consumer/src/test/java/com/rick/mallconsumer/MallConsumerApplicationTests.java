5448e68839c86b08bc549e489f74e18ff89b2dc7
wocenidema <120168080+wocenidema@users.noreply.github.com>
2023-09-17 17:37:01 +0800

    first push

---
+++ mall-consumer/src/test/java/com/rick/mallconsumer/MallConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.mallconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
